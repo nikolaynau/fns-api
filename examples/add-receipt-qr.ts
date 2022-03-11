a888d7774e56e28ab3b2c8b6cb027b8e2271d33c
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2022-03-11 18:34:10 +0300

    Fix examples

--- examples/add-receipt-qr.ts
+++ examples/add-receipt-qr.ts
@@ -2,15 +2,7 @@ import axios from 'axios';
 import * as fns from '..';
 
 const sessionId = '<your session id>';
-
-const fiscalData: fns.FiscalData = {
-  date: '2021-06-14T14:32',
-  operationType: 1,
-  sum: 43600,
-  fsId: '9982450301247855',
-  documentId: 65724,
-  fiscalSign: '7634185632'
-};
+const qr = '<your qr data scanned from the receipt>';
 
 const axiosInstance = axios.create({
   baseURL: fns.BASE_URL,
@@ -19,6 +11,6 @@ const axiosInstance = axios.create({
 const receiptApi = new fns.ReceiptApi(axiosInstance, sessionId);
 
 receiptApi
-  .addReceipt({ fiscalData, sendToEmail: false })
+  .addReceiptQR({ qr })
   .then((response) => console.log(response.data))
   .catch((e) => console.error(e));
